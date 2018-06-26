b003503f24980ca2de0125c9160b4444523c51e1
Tell Marcus de Souza Moitas <tellmoitas@gmail.com>
2018-06-26 08:49:41 -0300

    up

---
+++ src/FizzBuzz.java
@@ -0,0 +1,19 @@
+/**
+ * Created by tell on 25/06/2018.
+ */
+public class FizzBuzz implements IFizzBuzz
+{
+    public string Answer(int i)
+    {
+        throw new NotImplementedException();
+    }
+}
+public interface IFizzBuzz
+{
+    /// <summary>
+    /// Implemente o Jogo usando a técnica TDD
+    /// </summary>
+    /// <param name="number">um número da sequência do jogo</param>
+    /// <returns>resposta apropriada para o corrente número</returns>
+    public String Answer(int number);
+}
---
+++ tdd/src/TestFibonacci.java
@@ -0,0 +1,10 @@
+/**
+ * Created by tell on 26/06/2018.
+ */
+public class TestFibonacci {
+    public static void main(String[] args) {
+        int fib(int n){
+            return 0;
+        }
+    }
+}
